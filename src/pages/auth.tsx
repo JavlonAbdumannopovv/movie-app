c83506eb4c12e536bff6e8ce225e6511110f6921
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-17 19:55:14 +0500

    Header Component

---
+++ src/pages/auth.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const Auth = () => {
+  return (
+    <>
+      <Head>
+        <title>Movie App | Auth</title>
+        <meta name="description" content="For watching movies should sign to app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Auth</div>
+    </>
+  );
+};
+
+export default Auth;
