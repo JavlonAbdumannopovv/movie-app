48bdfba8ee58f1695c2eb304449a91e9efe69c49
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-27 18:23:18 +0500

    firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjyHVDgCELrAHBEQ42BIIHZqcqMT5y8Po",
+  authDomain: "movie-app-2d141.firebaseapp.com",
+  projectId: "movie-app-2d141",
+  storageBucket: "movie-app-2d141.appspot.com",
+  messagingSenderId: "197027779559",
+  appId: "1:197027779559:web:73efaffe8b4e832eb2e342",
+  measurementId: "G-XJXY9D8S3M",
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { db, auth };
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,16 +1,91 @@
-import Head from 'next/head';
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 
 const Auth = () => {
-	return (
-		<>
-			<Head>
-				<title>Auth</title>
-				<meta name='description' content='For watching movies you should sign to app' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-		</>
-	);
+  const [auth, setAuth] = useState<"signup" | "signin">("signin");
+
+  const toggleAuth = (state: "signin" | "signup") => {
+    setAuth(state);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Auth</title>
+        <meta
+          name="description"
+          content="For watching movies you should sign to app"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image
+        src={"/logo.svg"}
+        alt={"logo"}
+        width={70}
+        height={70}
+        className={"absolute left-4 top-4 cursor-pointer object-contain"}
+      />
+
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:mx-14">
+        <h1 className="text-4xl font-semibold">
+          {auth === "signup" ? "Sign up" : "Sign in"}
+        </h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="text" placeholder="Email" className="input" />
+          </label>
+          <label className="inline-block w-full">
+            <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+
+        {auth === "signin" ? (
+          <button
+            type="submit"
+            className="w-full bg-[#E10856] py-3 font-semibold"
+          >
+            {" "}
+            Sign In
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="w-full bg-[#E10856] py-3 font-semibold"
+          >
+            {" "}
+            Sign Up
+          </button>
+        )}
+
+        {auth === "signin" ? (
+          <div className="text-[gray]">
+            Not yet account?
+            <button
+              type="button"
+              className="text-white hover:underline pl-2"
+              onClick={() => toggleAuth("signup")}
+            >
+              Sign Up Now
+            </button>
+          </div>
+        ) : (
+          <div className="text-[gray]">
+            Already have account?
+            <button
+              type="button"
+              className="text-white hover:underline pl-2"
+              onClick={() => toggleAuth("signin")}
+            >
+              Sign In
+            </button>
+          </div>
+        )}
+      </form>
+    </>
+  );
 };
 
 export default Auth;
