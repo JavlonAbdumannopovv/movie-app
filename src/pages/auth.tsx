f7e027553e9546b2d42200d812409a7495762c55
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-07-07 13:37:08 +0500

    Zustand

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -13,7 +13,7 @@ const Auth = () => {
 	const router = useRouter();
 
 	if (user) router.push('/');
-	if (!isLoading) return <>Loading...</>;
+	if (isLoading) return <>Loading...</>;
 
 	const toggleAuth = (state: 'signup' | 'signin') => {
 		setAuth(state);
