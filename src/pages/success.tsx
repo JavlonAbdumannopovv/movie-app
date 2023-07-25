160ee576b8b8b15fb067204c6bcd8e743a2ca10e
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-07-25 11:49:44 +0500

    Success and Cancel page

---
+++ src/pages/success.tsx
@@ -0,0 +1,31 @@
+import { AiFillCheckCircle } from "react-icons/ai";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Cancel = () => {
+  return (
+    <>
+      <div className="flex justify-start py-2 px-4">
+        <Image
+          src={"/logo.svg"}
+          alt={"logo"}
+          width={56}
+          height={56}
+          className={"cursor-pointer object-contain"}
+        />
+      </div>
+      <div className="h-[90vh] flex flex-col justify-center items-center">
+        <AiFillCheckCircle className="w-20 h-20 text-green-500" />
+        <h1 className="text-2xl md:text-5xl mt-4">Subscription Complete</h1>
+        <Link href={"/"}>
+          <button className="mt-4 bg-green-500 py-4 px-5 rounded">
+            Dashboard
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Cancel;
